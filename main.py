b1b64aa39814f2ca9b572ccfe05503b6fbce428f
Jason Shockness <51177417+jshockness@users.noreply.github.com>
2022-06-14 17:26:24 +0000

    create player, create cars, detect if car and player collide

--- main.py
+++ main.py
@@ -6,9 +6,30 @@ from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
+screen.title("Turtle Crossing")
 screen.tracer(0)
+
+player = Player()
+cars = CarManager()
+
+screen.listen()
+screen.onkey(player.go_up, "Up")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
-    screen.update()
+  time.sleep(0.1)
+  screen.update()
+
+  cars.create_car()
+  cars.move_cars()
+
+  # Detect if turtle collides with a car
+  for car in cars.all_cars:
+    if car.distance(player) < 20:
+      game_is_on = False
+
+  # Detect if player finished
+  if player.is_finished():
+    player.goto_start()
+
+screen.exitonclick()
